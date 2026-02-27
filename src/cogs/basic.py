265130d5a12e67735bcfb4700d4a87bf950a9116
rayaso-0 <kbarre30@charlotte.edu>
2026-02-27 16:30:25 -0500

    added help, tos, and privacy policy commands

--- src/cogs/basic.py
+++ src/cogs/basic.py
@@ -19,10 +19,6 @@ class Basic(commands.Cog):
     async def goodmorning(self, interaction: discord.Interaction):
         await interaction.response.send_message(f"Good morning, {interaction.user.mention}!")
 
-    @app_commands.command(name="github", description="Link to our GitHub Repository")
-    async def github(self, interaction: discord.Interaction):
-        await interaction.response.send_message("Here is the link to our GitHub Repository: https://github.com/rayaso-0/ismDex_v2")
-
     @app_commands.command(name="touch", description="Touch Jalen")
     async def touch(self, interaction: discord.Interaction):
         await interaction.response.send_message(f"Yes, WE {interaction.user.mention}, are gonna touch jalen :money_mouth:")
@@ -34,6 +30,36 @@ class Basic(commands.Cog):
             await interaction.response.send_message("Heads!")
         else:
             await interaction.response.send_message("Tails!")
+
+    @app_commands.command(name="help", description="List of all commands")
+    async def help(self, interaction: discord.Integration):
+        await interaction.response.send_message("Here is a list of every current command:\n"
+                                                "`/hello`\n"
+                                                "`/goodmorning`\n"
+                                                "`/github`\n"
+                                                "`/tos`\n"
+                                                "`/privacypolicy`\n"
+                                                "`/touch`\n"
+                                                "`/flip`\n"
+                                                "`/ping`\n"
+                                                "`/groovy_bio`\n"
+                                                "`/jalen_bio`\n"
+                                                "`/rayaso_bio`\n"
+                                                "`/airthyus_bio`\n"
+                                                "`/ism_links`\n"
+                                                "`/help`\n")
+    
+    @app_commands.command(name="github", description="Link to our GitHub Repository")
+    async def github(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Here is the link to my GitHub Repository: https://github.com/rayaso-0/ismDex_v2")
+    
+    @app_commands.command(name="tos", description="Link to the ismDex discord bot Terms of Service")
+    async def github(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Here is the link to my Terms of Service: https://github.com/rayaso-0/ismDex_v2/blob/main/TERMS-OF-SERVICE.md")
+
+    @app_commands.command(name="privacypolicy", description="Link to the ismDex discord bot Privacy Policy")
+    async def github(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Here is the link to my Privacy Policy: https://github.com/rayaso-0/ismDex_v2/blob/main/PRIVACYPOLICY.md")
 
 async def setup(bot):
     await bot.add_cog(Basic(bot))
